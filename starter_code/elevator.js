3866446775cd809025dc4126e11594335e2f7b9c
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-09-04 19:40:52 +0200

    got the elevator to move and to pick up passenger

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,85 @@
 class Elevator {
+
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction;
+    this.timerId;
+    this.waitingList = [];
+    this.passengers = [];
+    this.requests = [];
+  }
+  //
+  // start() {
+  //   this.timerId = setInterval(this.update, 1000);
+  // }
+
+  start() {
+    this.timerId = setInterval(() => {
+      this.update();
+    }, 1000);
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  stop() {
+    clearInterval(this.timerId);
+  }
+
+  update() {
+
+
+    if (this.floor < this.waitingList[0].originFloor){
+      this.floorUp();
+      if (this.floor === this.waitingList[0].originFloor){
+        this._passengersEnter(this.waitingList[0]);
+      }
+    }
+    else {
+      this.floorDown();
+    }
+
+
+
+    this.log();
+  }
+
+  _passengersEnter(pepito) {
+    this.passengers.push(pepito);
+    this.waitingList.shift(pepito);
+    console.log(`${pepito.name} enetered the elevator`);
+
+   }
+
+  _passengersLeave(pepito) {
+    if(pepito.destinationFloor === this.floor){
+      console.log(`${pepito.name} left the elevator`);
+      this.passengers.pop(pepito);
+    }
+  }
+
+  floorUp() {
+    if(this.floor<this.MAXFLOOR){
+      this.floor++;
+      this.direction = "UP";
+    } else {this.floorDown();}
+  }
+
+  floorDown() {
+    if(this.floor>0){
+      this.floor--;
+      this.direction = "DOWN";
+    } else {this.floorUp();}
+  }
+
+  call(pepito) {
+    this.waitingList.push(pepito);
+    this.requests.push(pepito);
+  }
+
+  log() {
+    console.log(`Direction:  ${this.direction} | Floor:  ${this.floor}`);
+  }
+
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,11 @@
 const Elevator = require('./elevator.js');
+
+const Person = require('./person.js');
+
+const newElevator = new Elevator();
+const jennie = new Person('Jennie', 3, 9);
+const linus = new Person('Linus', 10, 0);
+
+
+newElevator.start();
+newElevator.call(jennie);
